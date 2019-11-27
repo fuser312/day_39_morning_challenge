eb6937fe71aad111c2d103bc1dabd37bc6dc527f
Sahil Singh <sahil.dogstar@gmail.com>
2019-11-27 12:08:29 +0530

    Dy 39 morning challenge completed.

--- bin/main.dart
+++ bin/main.dart
@@ -4,5 +4,48 @@
 // input: [[3,4],[1,2],[]]
 // output: [[[1,3,4],[2],[]], [[3,4],[2],[1]], [[4],[1,2],[3]]]
 
+List<List<List<int>>> hanoi(List<List<int>> inputState) {
+  List<List<List<int>>> finalState = [];
+
+  for (int i = 0; i < inputState.length; i++) {
+    for (int j = 0; j < inputState.length; j++) {
+      
+
+      if (isValidMove(inputState, i, j)) {
+        List<List<int>> copyInputState = [];
+        for (List <int> list in inputState) {
+          List <int> tempList = List.from(list);
+          copyInputState.add(tempList);
+        }
+        copyInputState[j].insert(0, copyInputState[i][0]);
+        
+        copyInputState[i].removeAt(0);
+        
+
+        finalState.add(copyInputState);
+      }
+    }
+  }
+  return finalState;
+}
+
+bool isValidMove(List<List<int>> checkList, int from, int to) {
+  if (checkList[from].isEmpty) {
+    return false;
+  }
+
+  if (checkList[to].isEmpty) {
+    return true;
+  }
+
+  return (checkList[from].first < checkList[to].first);
+}
+
 main() {
+  print(hanoi([
+    [3, 4],
+    [1, 2],
+    []
+  ]));
+  print(hanoi([[], [2,3], [4,1]]));
 }
--- test/Day_39_Morning_Challenge_test.dart
+++ test/Day_39_Morning_Challenge_test.dart
@@ -1,6 +1,11 @@
 import 'package:test/test.dart';
+import '../bin/main.dart';
 
 void main() {
   test('Test Case 1', () {
+    expect(hanoi(([[], [2,3], [4,1]])), [[[2], [3], [4, 1]], [[], [3], [2, 4, 1]], [[4], [2, 3], [1]]]
+
+);
+  expect(isValidMove([[], [3,4]],0,0), false);
   });
 }
